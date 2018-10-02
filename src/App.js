1b1a3ddbc90584d48604ead9a72bb4ba4c79261b
Tiiso <theotiiso@gmail.com>
2018-10-02 13:25:03 +0200

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap'
+import { DISHES } from './shared/dishes';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
 
 class App extends Component {
   render() {
@@ -13,11 +23,12 @@ class App extends Component {
           <NavbarBrand href="/">Ristorante con fusion</NavbarBrand>
         </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
       
     );
   }
 }
+
 
 export default App;
