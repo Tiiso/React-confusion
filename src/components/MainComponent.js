e4066528f6e8a184bcc56c6165dfc3134c955bdd
Tiiso <theotiiso@gmail.com>
2018-11-20 09:19:42 +0200

    React Animations

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -13,8 +13,8 @@ import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { actions } from 'react-redux-form';
-import { addComment, fetchDishes } from '../redux/ActionCreators';
-import { addComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
+import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 
 const mapStateToProps = state => {
@@ -27,7 +27,7 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = dispatch => ({
-  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+  postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
   fetchDishes: () => { dispatch(fetchDishes())},
   resetFeedbackForm: () => { dispatch(actions.reset('feedback'))},
   fetchComments: () => dispatch(fetchComments()),
@@ -48,6 +48,7 @@ const HomePage = () => {
     const DishWithId = ({match}) => {
       return(
           <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            postComment={this.props.postComment}
             comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
       );
     };
@@ -105,20 +106,23 @@ class Main extends Component {
           />
       );
     };
-    }
 
     return (
       <div>
         <Header />
         <div>
-          <Switch>
-              <Route path='/home' component={HomePage} />
-              <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
-              <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />}/>
-              <Route path='/menu/:dishId' component={DishWithId} />
-              <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm}/>} />
-              <Redirect to="/home" />
-          </Switch>
+          <TransitionGroup>
+            <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+              <Switch location={this.props.location}>
+                  <Route path='/home' component={HomePage} />
+                  <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />} />
+                  <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
+                  <Route path='/menu/:dishId' component={DishWithId} />
+                  <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
+                  <Redirect to="/home" />
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
         </div>
         <Footer />
       </div>
--- src/components/menuComponent.js
+++ src/components/menuComponent.js
@@ -7,7 +7,7 @@ import { baseUrl } from '../shared/baseUrl';
     function RenderMenuItem ({dish, onClick}) {
         return (
             <Card>
-                <Link to={`/menu/${dish.id}`} >
+                <Link to={`/menu/${dish.id}`}>
                     <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                     <CardImgOverlay>
                         <CardTitle>{dish.name}</CardTitle>
@@ -17,54 +17,57 @@ import { baseUrl } from '../shared/baseUrl';
         );
     }
 
-    const menu = props.dishes.dishes.map((dish) => {
+    const Menu = (props) => {
 
-        const menu = props.dishes.map((dish) => {
+        const menu = props.dishes.dishes.map((dish) => {
             return (
                 <div className="col-12 col-md-5 m-1"  key={dish.id}>
-                    <RenderMenuItem dish={dish} onClick={props.onClick} />
+                    <RenderMenuItem dish={dish} />
                 </div>
             );
         });
-        
-         if (props.dishes.isLoading) {
-            return(
+
+        if(props.dishes.isLoading) {
+            return (
                 <div className="container">
-                    <div className="row">            
+                    <div className="row">
                         <Loading />
                     </div>
                 </div>
             );
         }
-        else if (props.dishes.errMess) {
-            return(
+        else if(props.dishes.errMess) {
+            return (
                 <div className="container">
-                    <div className="row"> 
-                        <div className="col-12">
-                            <h4>{props.dishes.errMess}</h4>
-                        </div>
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
                     </div>
                 </div>
             );
         }
-        else
-         return (
-            <div className="container">
+        else {
+            return (
+                <div className="container">
                 <div className="row">
                     <Breadcrumb>
-                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                        <BreadcrumbItem>
+                            <Link to='/home'>Home</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem active>
+                            Menu
+                        </BreadcrumbItem>
                     </Breadcrumb>
                     <div className="col-12">
                         <h3>Menu</h3>
                         <hr />
-                    </div>                
+                    </div>
                 </div>
-                <div className="row">
-                    {menu}
+                    <div className="row">
+                        {menu}
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
 
 export default Menu;
